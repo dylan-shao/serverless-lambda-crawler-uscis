cacd69f3d95ce7575ca7933174842d292295f60b
Dylan-Shao <yangshao2014@gmail.com>
2018-09-01 00:30:36 -0700

    sms notification by nexmo

---
+++ README.md
@@ -0,0 +1,17 @@
+## What is this?
+
+Crawl USCIS case status and send SMS notification using [AWS lambda](https://aws.amazon.com/lambda/), [CloudWatch](https://aws.amazon.com/cloudwatch/) & [Serverless](https://serverless.com/) & [Nexmo](https://www.nexmo.com/)
+
+### How to use it
+
+1. You need to set up your [AWS](https://aws.amazon.com/) account, and [Serverless](https://serverless.com/) frameowrk, and create a secrets file and save the following variables:
+
+   * caseNumber
+   * NEXMO_API_KEY (from Nexmo)
+   * NEXMO_API_SECRET (from Nexmo)
+   * FROM (from Nexmo)
+   * MY_PHONE_NUMBER
+
+2. Then you need to [config your Serverless](https://serverless.com/framework/docs/providers/aws/guide/quick-start/), and [**deploy**](https://serverless.com/framework/docs/providers/aws/guide/deploying/) the lambda to your AWS.
+
+3. Set up the crawl intervals using [CloudWatch Events](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html)
--- handler.js
+++ handler.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { caseNumber } = require('./vault.js');
+const Nexmo = require('nexmo');
+const {
+  caseNumber,
+  NEXMO_API_KEY,
+  NEXMO_API_SECRET,
+  MY_PHONE_NUMBER,
+  FROM,
+} = require('./secrets.js');
 
 const getInfoFromHTML = html => {
   const $ = cheerio.load(html);
@@ -12,9 +19,18 @@ const getInfoFromHTML = html => {
 
 module.exports.getCaseStatus = async (event, context) => {
   const url = `https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=${caseNumber}`;
-
   const response = await axios.get(url);
+
   const { title, content } = getInfoFromHTML(response.data);
+
+  const nexmo = new Nexmo({
+    apiKey: NEXMO_API_KEY,
+    apiSecret: NEXMO_API_SECRET,
+  });
+
+  // free trail Nexmo cannot send title & content together, too long
+  // const msg = title + content;
+  nexmo.message.sendSms(FROM, MY_PHONE_NUMBER, title);
 
   return {
     statusCode: 200,
